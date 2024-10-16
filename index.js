b245749c2cc403ad034991ed9230a72a439a5a1a
Dhananjay <dhananjaysable370@gmail.com>
2024-10-16 23:53:04 +0530

    JavaScript

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let arr = [1, 2, 3, 4, 5, 6];
-const d = arr.filter((number,index) => {
-    return number % index === 0;
-})
+// let arr = [1, 2, 3, 4, 5, 6];
 
-console.log(d);
+// let func = (arr) => {
+//     return arr[0];
+// }
+
+// console.log(func(arr));
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// for (num in arr) {
+//     console.log(arr[num])
+// }
+
+// for (num of arr) {
+//     console.log(arr[num])
+// }
+// const d = arr.filter((number, index) => {
+//     return number % index === 0;
+// })
+
+// console.log(d);
+// console.log(i) //undefined
+// var i = 3;
+// console.log(j) //ReferenceError: Cannot access 'j' before initialization
+// let j = 3;
+
